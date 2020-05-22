b686d10d1e54ff913046ad30bb28659838e56612
re5nardo <re5nardo@naver.com>
2020-05-22 22:34:48 +0900

    [배인수] 1. [클라] Entrance씬에서 Photon & Playfab 로그인 및 로비 입장 로직 구현 2. [클라] LOP Application 추가 및 초기화 로직 구현

---
+++ Assets/Scripts/Entrance/Entrance.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Entrance : PunBehaviour
+{
+    [SerializeField] private GameObject goCI;
+    [SerializeField] private GameObject goIntro;
+    [SerializeField] private LoginComponent loginComponent;
+	[SerializeField] private Text textState;
+    [SerializeField] private GameObject goBtnJoinLobby;
+
+    #region MonoBehaviour
+    private IEnumerator Start()
+    {
+        //yield return StartCoroutine(PlayCI());
+
+        //yield return StartCoroutine(PlayIntro());
+
+        goBtnJoinLobby.SetActive(false);
+        textState.gameObject.SetActive(true);
+
+        textState.text = "Login중입니다.";
+
+        loginComponent.successCallback = () =>
+        {
+            Debug.Log("Congratulations, you made your first successful API call!");
+
+            ConnectToMasterServer();
+        };
+
+        loginComponent.errorCallback = (error) =>
+        {
+            Debug.LogWarning("Something went wrong with your first API call.  :(");
+            Debug.LogError("Here's some debug information:");
+            Debug.LogError(error.GenerateErrorReport());
+        };
+
+        loginComponent.StartLogin();
+
+        yield break;
+    }
+    #endregion  
+
+    private IEnumerator PlayCI()
+    {
+        goCI.SetActive(true);
+
+        yield return new WaitForSeconds(2f);
+
+        goCI.SetActive(false);
+    }
+
+    private IEnumerator PlayIntro()
+    {
+        goIntro.SetActive(true);
+
+        yield return new WaitForSeconds(2f);
+
+        goIntro.SetActive(false);
+    }
+
+    private IEnumerator WaitForInitialize()
+    {
+        textState.text = "Application 초기화중입니다.";
+
+        yield return new WaitUntil(() => LOP.Application.IsInitialized);
+
+        textState.gameObject.SetActive(false);
+        goBtnJoinLobby.SetActive(true);
+    }
+
+    private void ConnectToMasterServer()
+    {
+        textState.text = "마스터 서버에 접속중입니다.";
+
+		PhotonNetwork.ConnectUsingSettings("v1.0");
+    }
+
+	private void ConnectToLobby()
+    {
+        textState.text = "로비에 접속중입니다.";
+
+		PhotonNetwork.JoinLobby();
+    }
+
+    #region MonoBehaviourPunCallbacks
+	public override void OnFailedToConnectToPhoton(DisconnectCause cause)
+    {
+        textState.text = string.Format("마스터 서버 접속에 실패했습니다, cause : {0}", cause.ToString());
+    }
+
+    public override void OnConnectionFail(DisconnectCause cause)
+    {
+		Debug.LogError(string.Format("[Photon OnConnectionFail] cause : {0}", cause.ToString()));
+    }
+
+	public override void OnConnectedToMaster ()
+	{
+        textState.text = "마스터 서버에 접속하였습니다.";
+
+		foreach(TypedLobbyInfo lobbyInfo in PhotonNetwork.LobbyStatistics)
+		{
+			Debug.Log(string.Format("[Lobby Info] Type : {0}, IsDefault : {1}, Name : {2}, RoomCount : {3}, PlayerCount : {4}", lobbyInfo.Type, lobbyInfo.IsDefault, lobbyInfo.Name, lobbyInfo.RoomCount, lobbyInfo.PlayerCount));
+		}
+
+        StartCoroutine(WaitForInitialize());
+    }
+
+	public override void OnJoinedLobby ()
+	{
+        textState.text = "로비에 접속하였습니다.";
+
+		SceneManager.LoadScene("Lobby");
+	}
+    #endregion
+
+    #region Event Handler
+    public void OnJoinLobbyBtnClicked()
+    {
+        ConnectToLobby();
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Global/LOP.Application.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LOP
+{
+    public class Application
+    {
+        private static bool isInitialized = false;
+        public static bool IsInitialized { get { return isInitialized; } }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void OnBeforeSceneLoadRuntimeMethod()
+        {
+            Initialize();
+        }
+
+        private static void Initialize()
+        {
+            isInitialized = true;
+        }
+
+//        private static IEnumerator _Initialize()
+//        {
+//        //  Target FrameRate
+//#if UNITY_EDITOR
+//        UnityEngine.Application.targetFrameRate = 30;
+//#endif
+
+//            CatalogData.Instance.UpdateData();
+//            UserProfileData.Instance.UpdateData();
+
+//            yield return new WaitUntil(() => CatalogData.Instance.IsCached());
+//            yield return new WaitUntil(() => UserProfileData.Instance.IsCached());
+
+//            isInitialized = true;
+//        }
+    }
+}
