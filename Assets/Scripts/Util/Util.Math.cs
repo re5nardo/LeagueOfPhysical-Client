d21f6a16f039e451a7959fbf3c78292284e1982b
re5nardo <re5nardo@naver.com>
2020-09-13 20:58:59 +0900

    [배인수] 1. Util.Math 분리

---
+++ Assets/Scripts/Util/Util.Math.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.IO.Compression;
+
+public partial class Util
+{
+    public class Math
+    {
+        public static float Constant_E = 2.718282f;
+
+        //	https://answers.unity.com/questions/1032673/how-to-get-0-360-degree-from-two-points.html
+        public static float FindDegree(Vector2 vec2Position)
+        {
+            float value = (float)((Mathf.Atan2(vec2Position.x, vec2Position.y) / Mathf.PI) * 180f);
+            if (value < 0)
+                value += 360f;
+
+            return value;
+        }
+    }
+}
--- Assets/Scripts/Util/Util.cs
+++ Assets/Scripts/Util/Util.cs
@@ -6,19 +6,6 @@ using System.IO.Compression;
 
 public partial class Util
 {
-    public class Math
-    {
-		//	https://answers.unity.com/questions/1032673/how-to-get-0-360-degree-from-two-points.html
-		public static float FindDegree(Vector2 vec2Position)
-	    {
-			float value = (float)((Mathf.Atan2(vec2Position.x, vec2Position.y) / Mathf.PI) * 180f);
-	    	if(value < 0)
-	    		value += 360f;
-
-			return value;
-	    }
-    }
-
     public static void Parse(string text, char delim, List<string> output)
     {
         string[] arrText = text.Split(delim);
