4acf4fc9299704d966ece0ac27b0fe108cd0e59c
re5nardo <re5nardo@naver.com>
2020-11-02 01:56:45 +0900

    [배인수] 1. 소스 다듬기

--- Assets/Scripts/Factory/BehaviorFactory.cs
+++ Assets/Scripts/Factory/BehaviorFactory.cs
@@ -7,29 +7,24 @@ using GameFramework;
 
 public class BehaviorFactory : MonoSingleton<BehaviorFactory>
 {
+    private List<Type> source = new List<Type>
+    {
+        typeof(Move),
+        typeof(Rotation),
+        typeof(MeleeAttack),
+        typeof(RangeAttack),
+    };
+
     public BehaviorBase CreateBehavior(GameObject goTarget, int nBehaviorMasterID)
     {
         try
         {
             MasterData.Behavior masterData = MasterDataManager.instance.GetMasterData<MasterData.Behavior>(nBehaviorMasterID);
 
-            switch (masterData.ClassName)
+            Type target = source.Find(type => type.Name == masterData.ClassName);
+            if (target != null)
             {
-                case "Move":
-                    Move move = goTarget.AddComponent<Move>();
-                    return move;
-
-                case "Rotation":
-                    Rotation rotation = goTarget.AddComponent<Rotation>();
-                    return rotation;
-
-                case "MeleeAttack":
-                    MeleeAttack meleeAttack = goTarget.AddComponent<MeleeAttack>();
-                    return meleeAttack;
-
-                case "RangeAttack":
-                    RangeAttack rangeAttack = goTarget.AddComponent<RangeAttack>();
-                    return rangeAttack;
+                return goTarget.AddComponent(target) as BehaviorBase;
             }
 
             Debug.LogError(string.Format("There is no matched ClassName! masterData.ClassName : {0}", masterData.ClassName));
