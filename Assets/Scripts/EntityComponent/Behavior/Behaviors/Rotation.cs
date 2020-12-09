2d454babc2dfd4f84ddade8e9bf670ada81b8218
re5nardo <re5nardo@naver.com>
2020-12-09 19:22:41 +0900

    [배인수] 1. 로테이션 관련 다듬기

--- Assets/Scripts/EntityComponent/Behavior/Behaviors/Rotation.cs
+++ Assets/Scripts/EntityComponent/Behavior/Behaviors/Rotation.cs
@@ -16,32 +16,15 @@ namespace Behavior
         #region BehaviorBase
         protected override bool OnBehaviorUpdate()
         {
-            float dest_y = Quaternion.LookRotation(m_vec3Direction).eulerAngles.y;
-            float mine_y = Entity.Rotation.y;
-
-            float toRotate = dest_y - mine_y;
-            if (Util.Approximately(toRotate, 0))
-                return true;
-
-            int sign = GetRotationSign(Entity.Forward, m_vec3Direction);
-            if (sign > 0)
+            float toRotate = Vector3.SignedAngle(m_vec3Direction, Entity.Forward, Vector3.up);
+            if (toRotate == 0)
             {
-                if (dest_y < mine_y)
-                {
-                    toRotate = (dest_y + 360) - mine_y;
-                }
-            }
-            else
-            {
-                if (dest_y > mine_y)
-                {
-                    toRotate = dest_y - (mine_y + 360);
-                }
+                return false;
             }
 
-            Entity.AngularVelocity = new Vector3(0, sign * m_fAngularSpeed, 0);
+            Entity.AngularVelocity = new Vector3(0, (toRotate > 0 ? 1 : -1) * m_fAngularSpeed, 0);
 
-            float rotated = Entity.AngularVelocity.y * DeltaTime;
+            float rotated = toRotate * m_fAngularSpeed * DeltaTime;
 
             if (Util.Approximately(toRotate, rotated) || Mathf.Abs(toRotate) <= Mathf.Abs(rotated))
             {
@@ -55,6 +38,13 @@ namespace Behavior
 
                 return true;
             }
+        }
+
+        protected override void OnBehaviorEnd()
+        {
+            base.OnBehaviorEnd();
+
+            Entity.AngularVelocity = Vector3.zero;
         }
 
         public override void SetData(int nBehaviorMasterID, params object[] param)
@@ -75,14 +65,6 @@ namespace Behavior
         public void SetDirection(Vector3 vec3Direction)
         {
             m_vec3Direction = vec3Direction;
-        }
-
-        private int GetRotationSign(Vector3 cur, Vector3 dest)
-        {
-            cur = new Vector3(cur.x, 0, cur.z);
-            dest = new Vector3(dest.x, 0, dest.z);
-
-            return Vector3.Cross(cur, dest).y > 0 ? 1 : -1;
         }
     }
 }
