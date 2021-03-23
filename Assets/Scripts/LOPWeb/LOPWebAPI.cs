8a41d09972a3b1ae0f94892230abdfcccb5f6ed0
re5nardo <re5nardo@naver.com>
2021-03-24 01:09:48 +0900

    [배인수] 1. Http 모듈 구현 2. Web API 구현

---
+++ Assets/Scripts/LOPWeb/LOPWebAPI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class LOPWebAPI
+{
+    /// <summary>
+    /// Create a matchmaking ticket as a client.
+    /// </summary>
+    public static void CreateMatchmakingTicket(CreateMatchmakingTicketRequest request, Action<CreateMatchmakingTicketResult> resultCallback, Action<LOPHttpError> errorCallback, object customData = null, Dictionary<string, string> extraHeaders = null)
+    {
+        LOPHttp.MakeApiCall("/match/createMatchmakingTicket", request, resultCallback, errorCallback, customData, extraHeaders, LOPServerSettings.Get());
+    }
+
+    /// <summary>
+    /// Cancel a matchmaking ticket.
+    /// </summary>
+    public static void CancelMatchmakingTicket(CancelMatchmakingTicketRequest request, Action<CancelMatchmakingTicketResult> resultCallback, Action<LOPHttpError> errorCallback, object customData = null, Dictionary<string, string> extraHeaders = null)
+    {
+        LOPHttp.MakeApiCall("/match/cancelMatchmakingTicket", request, resultCallback, errorCallback, customData, extraHeaders, LOPServerSettings.Get());
+    }
+}
---
+++ Assets/Scripts/LOPWeb/Model/CreateMatchmakingTicketRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMatchmakingTicketRequest : LOPHttpRequestBase
+{
+    public string userId;
+    public string gameType;
+    public string matchType;
+    public int rating;
+}
