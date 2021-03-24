5db5bf67aa6575442ce5b5b05dba558b3b3a919b
re5nardo <re5nardo@naver.com>
2021-03-24 21:21:50 +0900

    [배인수] 1. Put overloading 함수 추가

--- Assets/Scripts/LOPHttp/LOPHttpTransport.cs
+++ Assets/Scripts/LOPHttp/LOPHttpTransport.cs
@@ -81,6 +81,11 @@ public class LOPHttpTransport : MonoBehaviour
     }
 
     //  Put
+    public void Put(string uri, Dictionary<string, string> requestHeaders, Action<string> onResult, Action<string> onError)
+    {
+        StartCoroutine(PutRoutine(uri, requestHeaders, onResult, onError));
+    }
+
     public void Put(string uri, byte[] bodyData, Dictionary<string, string> requestHeaders, Action<string> onResult, Action<string> onError)
     {
         StartCoroutine(PutRoutine(uri, bodyData, requestHeaders, onResult, onError));
@@ -89,6 +94,65 @@ public class LOPHttpTransport : MonoBehaviour
     public void Put(string uri, string bodyData, Dictionary<string, string> requestHeaders, Action<byte[]> onResult, Action<string> onError)
     {
         StartCoroutine(PutRoutine(uri, bodyData, requestHeaders, onResult, onError));
+    }
+
+    private IEnumerator PutRoutine(string uri, Dictionary<string, string> requestHeaders, Action<string> onResult, Action<string> onError)
+    {
+        using (var www = new UnityWebRequest(uri))
+        {
+            www.method = UnityWebRequest.kHttpVerbPUT;
+            www.downloadHandler = new DownloadHandlerBuffer();
+
+            foreach (var headerPair in requestHeaders)
+            {
+                if (!string.IsNullOrEmpty(headerPair.Key) && !string.IsNullOrEmpty(headerPair.Value))
+                {
+                    www.SetRequestHeader(headerPair.Key, headerPair.Value);
+                }
+                else
+                {
+                    Debug.LogWarning("Null header: " + headerPair.Key + " = " + headerPair.Value);
+                }
+            }
+
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError(www.error);
+                onError?.Invoke(www.error);
+            }
+            else
+            {
+                try
+                {
+                    string response = "";
+                    if (www.GetResponseHeader("Content-Encoding") == "gzip")
+                    {
+                        using (var memoryStream = new MemoryStream(www.downloadHandler.data))
+                        {
+                            using (var gZipStream = new GZipStream(memoryStream, CompressionMode.Decompress))
+                            {
+                                using (var streamReader = new StreamReader(gZipStream))
+                                {
+                                    response = streamReader.ReadToEnd();
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        response = www.downloadHandler.text;
+                    }
+
+                    onResult?.Invoke(response);
+                }
+                catch (Exception e)
+                {
+                    onError?.Invoke(e.Message);
+                }
+            }
+        }
     }
 
     private IEnumerator PutRoutine(string uri, byte[] bodyData, Dictionary<string, string> requestHeaders, Action<string> onResult, Action<string> onError)
