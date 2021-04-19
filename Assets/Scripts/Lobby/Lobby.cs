85c9f0d9f674192dad909b22e3b02492f17d3b92
re5nardo <re5nardo@naver.com>
2021-04-20 00:56:04 +0900

    [배인수] 1. 로비 MessageBroker Dispose 호출하지 않도록 수정

--- Assets/Scripts/Lobby/Lobby.cs
+++ Assets/Scripts/Lobby/Lobby.cs
@@ -4,9 +4,4 @@ using UniRx;
 public class Lobby : MonoBehaviour
 {
     public static readonly MessageBroker MessageBroker = new MessageBroker();
-
-    private void OnDestroy()
-    {
-        MessageBroker.Dispose();
-    }
 }
