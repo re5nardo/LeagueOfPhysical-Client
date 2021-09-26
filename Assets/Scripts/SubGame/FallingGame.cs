972f305dbe5eb91eaf150d7236c86924c3e6beb6
re5nardo <re5nardo@naver.com>
2021-09-26 18:53:07 +0900

    [배인수] 1. RotateObject 추가 2. FallingGame 추가

---
+++ Assets/Scripts/SubGame/FallingGame.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Entity;
+
+public class FallingGame : SubGameBase
+{
+    private void Start()
+    {
+        SceneMessageBroker.AddSubscriber<GameMessage.EntityRegister>(OnEntityRegister);
+    }
+
+    private void OnDestroy()
+    {
+        SceneMessageBroker.RemoveSubscriber<GameMessage.EntityRegister>(OnEntityRegister);
+    }
+
+    private void OnEntityRegister(GameMessage.EntityRegister message)
+    {
+        var entity = Entities.Get<LOPMonoEntityBase>(message.entityId);
+
+        entity.Rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    protected override IEnumerator OnInitialize()
+    {
+        yield return SceneManager.LoadSceneAsync(LOP.Game.Current.GameManager.mapName, LoadSceneMode.Additive);
+
+        foreach (var entity in Entities.GetAll<LOPMonoEntityBase>())
+        {
+            if (entity.EntityRole != EntityRole.Player)
+            {
+                continue;
+            }
+
+            entity.Rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+        }
+
+        Physics.gravity *= SubGameEnvironment.GravityFactor;
+    }
+
+    protected override void OnGameStart()
+    {
+    }
+
+    protected override void OnGameEnd()
+    {
+    }
+
+    protected override void OnTick(int tick)
+    {
+    }
+
+    protected override void OnEarlyTickEnd(int tick)
+    {
+    }
+}
