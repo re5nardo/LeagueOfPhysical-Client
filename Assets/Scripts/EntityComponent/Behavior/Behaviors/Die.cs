07ab05f233ebd288523fcf4bbe01c36d84155488
re5nardo <re5nardo@naver.com>
2021-11-05 00:53:49 +0900

    [배인수] 1. MasterData Generated Define 파일 생성 위치 수정 2. Die Behavior 추가

---
+++ Assets/Scripts/EntityComponent/Behavior/Behaviors/Die.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Entity;
+
+namespace Behavior
+{
+    public class Die : BehaviorBase
+    {
+        #region BehaviorBase
+        protected override void OnBehaviorStart()
+        {
+            base.OnBehaviorStart();
+
+            Entity.Rigidbody.detectCollisions = false;
+        }
+
+        protected override bool OnBehaviorUpdate()
+        {
+            return CurrentUpdateTime < MasterData.lifespan;
+        }
+
+        protected override void OnBehaviorEnd()
+        {
+            base.OnBehaviorEnd();
+
+            Entity.Rigidbody.detectCollisions = true;
+        }
+        #endregion
+    }
+}
