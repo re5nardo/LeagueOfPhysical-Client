8a96647fcd98b5105f337410c1d524582c0d97c8
re5nardo <re5nardo@naver.com>
2022-05-29 22:39:04 +0900

    ResponseCode 수정

--- Assets/Scripts/LOPWeb/ResponseCode.cs
+++ Assets/Scripts/LOPWeb/ResponseCode.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class ResponseCode
 {
     //  Success
-    public const int SUCCESS = 200;
+    public const int SUCCESS = 0;
 
     //  Match
     public const int INVALID_TO_MATCHMAKING = 300;
     public const int ALREADY_IN_GAME = 301;
+
+    #region User
+    public const int USER_NOT_EXIST = 30000;
+    #endregion
 
     public const int UNKNOWN_ERROR = 99999;
 }
