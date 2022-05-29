1cd315e0b2003eeb7d0d0b1f55084403a4b96e36
re5nardo <re5nardo@naver.com>
2022-05-29 22:59:21 +0900

    Http 모듈 수정된 내용 적용

--- Assets/Scripts/LOPWeb/LOPWebAPI.cs
+++ Assets/Scripts/LOPWeb/LOPWebAPI.cs
@@ -8,31 +8,31 @@ public class LOPWebAPI
 {
     public static HttpRequestContainer<JoinLobbyResult> JoinLobby(JoinLobbyRequest request, Action<JoinLobbyResult> onResult = null, Action<string> onError = null)
     {
-        return Http.Put("/joinLobby", JsonUtility.ToJson(request), onResult, onError, apiSettings: GameFramework.ServerSettings.Get("ServerSettings_Lobby"));
+        return Http.Put("joinLobby", JsonUtility.ToJson(request), onResult, onError, apiSettings: GameFramework.ServerSettings.Get("ServerSettings_Lobby"));
     }
 
     public static HttpRequestContainer<LeaveLobbyResult> LeaveLobby(LeaveLobbyRequest request, Action<LeaveLobbyResult> onResult = null, Action<string> onError = null)
     {
-        return Http.Put("/leaveLobby", JsonUtility.ToJson(request), onResult, onError, apiSettings: GameFramework.ServerSettings.Get("ServerSettings_Lobby"));
+        return Http.Put("leaveLobby", JsonUtility.ToJson(request), onResult, onError, apiSettings: GameFramework.ServerSettings.Get("ServerSettings_Lobby"));
     }
 
     public static HttpRequestContainer<CreateMatchmakingTicketResult> CreateMatchmakingTicket(CreateMatchmakingTicketRequest request, Action<CreateMatchmakingTicketResult> onResult = null, Action<string> onError = null)
     {
-        return Http.Put("/match/matchmakingTicket", JsonUtility.ToJson(request), onResult, onError, apiSettings: GameFramework.ServerSettings.Get("ServerSettings_Matchmaking"));
+        return Http.Put("match/matchmakingTicket", JsonUtility.ToJson(request), onResult, onError, apiSettings: GameFramework.ServerSettings.Get("ServerSettings_Matchmaking"));
     }
 
     public static HttpRequestContainer<CancelMatchmakingTicketResult> CancelMatchmakingTicket(string userId, Action<CancelMatchmakingTicketResult> onResult = null, Action<string> onError = null)
     {
-        return Http.Delete($"/match/matchmakingTicket/{userId}", onResult, onError, apiSettings: GameFramework.ServerSettings.Get("ServerSettings_Matchmaking"));
+        return Http.Delete($"match/matchmakingTicket/{userId}", onResult, onError, apiSettings: GameFramework.ServerSettings.Get("ServerSettings_Matchmaking"));
     }
 
     public static HttpRequestContainer<GetUserMatchStateResult> GetUserMatchState(string userId, Action<GetUserMatchStateResult> onResult = null, Action<string> onError = null)
     {
-        return Http.Get($"/users/matchState/{userId}", onResult, onError, apiSettings: GameFramework.ServerSettings.Get("ServerSettings_Lobby"));
+        return Http.Get($"users/matchState/{userId}", onResult, onError, apiSettings: GameFramework.ServerSettings.Get("ServerSettings_Lobby"));
     }
 
     public static HttpRequestContainer<GetRoomResult> GetRoom(string roomId, Action<GetRoomResult> onResult = null, Action<string> onError = null)
     {
-        return Http.Get($"/room/{roomId}", onResult, onError, apiSettings: GameFramework.ServerSettings.Get("ServerSettings_Lobby"));
+        return Http.Get($"room/{roomId}", onResult, onError, apiSettings: GameFramework.ServerSettings.Get("ServerSettings_Lobby"));
     }
 }
