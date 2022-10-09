94830e48eb0e251fb792be86f03512b1f12d237e
re5nardo <re5nardo@naver.com>
2022-10-09 22:37:43 +0900

    소스 다듬기

--- Assets/Scripts/SubGame/FlapWang/FlapWang.Contents.cs
+++ Assets/Scripts/SubGame/FlapWang/FlapWang.Contents.cs
@@ -22,6 +22,7 @@ public partial class FlapWang
         var entity = Entities.Get<LOPMonoEntityBase>(message.entityId);
 
         if (entity.EntityRole != EntityRole.Player
+            || !entity.HasAuthority
             || !(entity is Character character)
             || !character.IsAlive
             || character.HasStatusEffect(StatusEffect.Invincible)
--- Assets/Scripts/SubGame/FlapWang/FlapWang.cs
+++ Assets/Scripts/SubGame/FlapWang/FlapWang.cs
@@ -11,12 +11,6 @@ public partial class FlapWang : SubGameBase
         foreach (var entity in Entities.GetAll<LOPMonoEntityBase>())
         {
             entity.Rigidbody.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
-
-            if (entity.EntityRole == EntityRole.Player)
-            {
-                var behaviorController = entity.GetComponent<BehaviorController>();
-                behaviorController.StartBehavior(new ContinuousMoveBehaviorParam(Define.MasterData.BehaviorId.ContinuousMove, Vector3.right));
-            }
         }
 
         Physics.gravity *= LOP.Game.Current.MapData.mapEnvironment.GravityFactor;
@@ -39,6 +33,14 @@ public partial class FlapWang : SubGameBase
 
     protected override void OnGameStart()
     {
+        foreach (var entity in Entities.GetAll<LOPMonoEntityBase>())
+        {
+            if (entity.EntityRole == EntityRole.Player)
+            {
+                var behaviorController = entity.GetComponent<BehaviorController>();
+                behaviorController.StartBehavior(new ContinuousMoveBehaviorParam(Define.MasterData.BehaviorId.ContinuousMove, Vector3.right));
+            }
+        }
     }
 
     protected override void OnGameEnd()
